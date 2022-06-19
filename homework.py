1b93f2f7f7cc53b61e8cb0cfcc1cdbd38c43d181
Екатерина Алексеенк <katrinale@yandex.ru>
2022-06-19 10:26:12 +0300

    Внесла исправления

--- homework.py
+++ homework.py
@@ -59,7 +59,7 @@ def get_api_answer(current_timestamp):
 
 def check_response(response):
     """Проверяет ответ API на корректность."""
-    homeworks_list = response.get('homeworks')
+    homeworks_list = response.get('homeworks', None)
     if type(response) is not dict:
         msg = 'Ошибка словаря'
         logger.error(msg)
